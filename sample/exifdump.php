9168c03f644098886fdedfc735af4cd07538d888
yoya <yoya@g.awm.jp>
2016-06-06 16:26:27 +0900

    accept php://stdin as input file

--- sample/exifdump.php
+++ sample/exifdump.php
@@ -4,13 +4,22 @@ require_once 'IO/Exif.php';
 
 $options = getopt("f:hnv");
 
-if ((isset($options['f']) === false) || (is_readable($options['f']) === false)) {
+function usage() {
     fprintf(STDERR, "Usage: php exifdump.php -f <exif_file> [-n] [-h] [-v]\n");
     fprintf(STDERR, "ex) php exifdump.php -f test.exif -hnv \n");
+}
+
+if (isset($options['f']) === false) {
+    usage();
     exit(1);
 }
 
-$exifdata = file_get_contents($options['f']);
+$exiffile = $options['f'];
+if (($exiffile !== "php://stdin") && (is_readable($options['f']) === false)) {
+    usage();
+    exit(1);
+}
+$exifdata = file_get_contents($exiffile);
 
 $exif = new IO_Exif();
 $exif->parse($exifdata);

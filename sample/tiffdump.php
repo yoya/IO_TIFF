2bfa422c4abbc5de143eb962f4f1c4322e6077ab
yoya <yoya@yoyaMBP2016late.local>
2016-12-22 18:26:52 +0900

    tiffdump.php accept JPEG, Exif format.

--- sample/tiffdump.php
+++ sample/tiffdump.php
@@ -21,6 +21,42 @@ if (($tifffile !== "php://stdin") && (is_readable($options['f']) === false)) {
 }
 $tiffdata = file_get_contents($tifffile);
 
+if (substr($tiffdata, 0, 2) === "\xff\xd8") { // JPEG - SOI(Start of Image)
+    echo "JPEG Format:".PHP_EOL;
+    $cur = 2; // skip SOI marker
+    $found = false; // search APP1 - Exif
+    while (! $found) {
+        $marker1 = ord($tiffdata[$cur]);
+        $marker2 = ord($tiffdata[$cur+1]);
+        if ($marker1 !== 0xFF) {
+            echo "Broken chunk marker(".$marker1.") offset:$cur\n";
+            exit (1);
+        }
+        switch($marker2) {
+        case 0xE1: // APP1
+            if (substr($tiffdata, $cur+4, 6) === "Exif\0\0") {
+                $found = true;
+            }
+            break;
+        case 0xDA: // SOS
+        case 0xD9: // EOI (End of Image)
+            echo "Not found APP1 chunk.\n";
+            exit (1);
+            break;
+        }
+        $len = 0x100 * ord($tiffdata[$cur+2]) + ord($tiffdata[$cur+3]);
+        if ($found) {
+            $tiffdata = substr($tiffdata, $cur + 4, $len - 2/*marker field*/);
+        }
+        $cur += 2/*length field*/ + $len;
+    }
+}
+
+if (substr($tiffdata, 0, 6) === "Exif\0\0") {
+    echo "Exif Format:".PHP_EOL;
+    $tiffdata = substr($tiffdata, 6);
+}
+
 $tiff = new IO_TIFF();
 $tiff->parse($tiffdata);
 
